796b35651c70777b21ac4bc6cab425252126d076
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-05 02:12:17 -0300

    feat(entities): creates transfer repository interface

---
+++ backend/src/main/java/com/johnnycarreiro/fts/domain/entities/transfer/TransferRepository.java
@@ -0,0 +1,60 @@
+package com.johnnycarreiro.fts.domain.entities.transfer;
+
+import java.util.UUID;
+
+import com.johnnycarreiro.fts.core.domain.exceptions.DomainException;
+import com.johnnycarreiro.fts.core.utils.Result;
+import com.johnnycarreiro.fts.domain.entities.transfer_fee.TransferFee;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Repository interface for managing the lifecycle of the Transfer aggregate.
+ * Provides methods for persistence and querying domain-related data.
+ */
+public interface TransferRepository {
+
+  /**
+   * Saves a Transfer entity to the persistent store.
+   * If the entity already exists, it should be updated.
+   *
+   * @param transfer the Transfer entity to save or update.
+   * @return a Result indicating success or an error.
+   */
+  Result<Void, DomainException> save(Transfer transfer);
+
+  /**
+   * Finds a Transfer by its unique identifier.
+   *
+   * @param transferId the unique identifier of the Transfer.
+   * @return a Result containing the Transfer if found or an error if not.
+   */
+  Result<Transfer, DomainException> findById(UUID transferId);
+
+  /**
+   * Updates an existing Transfer entity.
+   * Should throw an exception if the entity does not exist.
+   *
+   * @param transfer the Transfer entity with updated information.
+   * @return a Result indicating success or an error.
+   */
+  Result<Void, DomainException> update(Transfer transfer);
+
+  /**
+   * Finds an applicable TransferFee based on the given amount and transfer type.
+   *
+   * @param amount       the amount of the transfer.
+   * @param transferType the type of the transfer.
+   * @return a Result containing the applicable TransferFee if found or an error
+   *         if not.
+   */
+  Result<TransferFee, DomainException> findApplicableFee(BigDecimal amount, String transferType);
+
+  /**
+   * Lists all TransferFee entities.
+   *
+   * @return a Result containing a list of TransferFee entities.
+   */
+  Result<List<TransferFee>, DomainException> listAllFees();
+}
