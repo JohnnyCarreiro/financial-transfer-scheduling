e4f32df205048becb4d464924cc082ccce13ef4a
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-05 09:06:41 -0300

    refactor(application): changes default run configs

--- backend/src/main/java/com/johnnycarreiro/fts/BackendApplication.java
+++ backend/src/main/java/com/johnnycarreiro/fts/BackendApplication.java
@@ -3,11 +3,13 @@ package com.johnnycarreiro.fts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.johnnycarreiro.fts.configurations.WebServerConfig;
+
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WebServerConfig.class, args);
+  }
 
 }
