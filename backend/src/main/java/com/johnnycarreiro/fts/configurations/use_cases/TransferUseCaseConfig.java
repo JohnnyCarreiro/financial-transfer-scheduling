d86f428d1e05bb549a96b72da1977ff4e7da7370
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-06 17:46:54 -0300

    fix(application)!: fixes mismatch dependency container registry

--- backend/src/main/java/com/johnnycarreiro/fts/configurations/use_cases/TransferUseCaseConfig.java
+++ backend/src/main/java/com/johnnycarreiro/fts/configurations/use_cases/TransferUseCaseConfig.java
@@ -1,3 +1,4 @@
+
 package com.johnnycarreiro.fts.configurations.use_cases;
 
 import org.springframework.context.annotation.Bean;
@@ -6,29 +7,24 @@ import org.springframework.context.annotation.Configuration;
 import com.johnnycarreiro.fts.application.transfer.create.CreateTransferUseCase;
 import com.johnnycarreiro.fts.domain.services.TransferFeeCalculatorService;
 import com.johnnycarreiro.fts.infra.transfer.JpaTransferRepository;
+import com.johnnycarreiro.fts.infra.transfer.persistence.TransferJpaRepository;
 
 @Configuration
 public class TransferUseCaseConfig {
 
-  private JpaTransferRepository transferRepository;
-  private TransferFeeCalculatorService feeCalculatorService;
+  private final JpaTransferRepository transferRepository;
 
-  // Setter Injection
-  public void setTransferRepository(JpaTransferRepository transferRepository) {
-    this.transferRepository = transferRepository;
-  }
-
-  public void setFeeCalculatorService(TransferFeeCalculatorService feeCalculatorService) {
-    this.feeCalculatorService = feeCalculatorService;
-  }
-
-  @Bean
-  public CreateTransferUseCase createTransferUseCase() {
-    return new CreateTransferUseCase(feeCalculatorService, transferRepository);
+  TransferUseCaseConfig(TransferJpaRepository jpaRepository) {
+    this.transferRepository = new JpaTransferRepository(jpaRepository);
   }
 
   @Bean
   public TransferFeeCalculatorService transferFeeCalculatorService() {
     return new TransferFeeCalculatorService();
+  }
+
+  @Bean
+  public CreateTransferUseCase createTransferUseCase(TransferFeeCalculatorService feeCalculatorService) {
+    return new CreateTransferUseCase(feeCalculatorService, transferRepository);
   }
 }
