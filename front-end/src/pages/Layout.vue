14c3723d1e08769e9ae5bc0ff0cda3e78fa3fd50
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-06 20:53:53 -0300

    feat(ui): creates pages default layout

---
+++ front-end/src/pages/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="min-h-screen flex flex-col bg-gray-100">
+    <NavigationHeader />
+
+    <main class="container mx-auto px-4 py-8">
+      <h1 class="text-2xl font-bold text-gray-800 mb-4">Bem-vindo às Transferências</h1>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script>
+import NavigationHeader from "@/components/NavigationHeader.vue";
+
+export default {
+	name: "TransferLayout",
+	components: {
+		NavigationHeader,
+	},
+};
+</script>
