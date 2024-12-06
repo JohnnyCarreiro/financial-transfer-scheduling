f0cc209436c79ce1a75370e2e52eccbcfe311535
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-06 17:56:13 -0300

    fix(infra)!: fixes persistence entities

--- backend/src/main/java/com/johnnycarreiro/fts/infra/transfer/persistence/TransferEntity.java
+++ backend/src/main/java/com/johnnycarreiro/fts/infra/transfer/persistence/TransferEntity.java
@@ -5,35 +5,66 @@ import java.time.Instant;
 
 import com.johnnycarreiro.fts.domain.entities.transfer.Transfer;
 import com.johnnycarreiro.fts.domain.entities.transfer_fee.TransferFee;
-import com.johnnycarreiro.fts.domain.value_objects.account.Account;
+import com.johnnycarreiro.fts.domain.value_objects.transfer_status.TransferStatus;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "transfers")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TransferEntity {
 
   @Id
+  @Column(name = "id", nullable = false)
   private String id;
 
+  @Column(name = "source_account", nullable = false)
   private String sourceAccount;
 
+  @Column(name = "destination_account", nullable = false)
   private String destinationAccount;
 
+  @Column(name = "amount", nullable = false)
   private BigDecimal amount;
 
+  @Column(name = "scheduled_date", nullable = false)
   private Instant scheduledDate;
 
+  @Column(name = "transfer_date")
+  private Instant transferDate;
+
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "transfer_fee_id", referencedColumnName = "id")
+  @JoinColumn(name = "transfer_fee_id", referencedColumnName = "id", nullable = false)
   private TransferFeeEntity transferFee;
 
-  // Getters and Setters
+  @Column(name = "status", nullable = false)
+  private String status;
+
+  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Instant createdAt;
+
+  @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Instant updatedAt;
+
+  @Column(name = "deleted_at", nullable = true, columnDefinition = "TIMESTAMP")
+  private Instant deletedAt;
+
+  @PreUpdate
+  public void preUpdate() {
+    this.updatedAt = Instant.now();
+  }
 
   public static TransferEntity fromDomain(Transfer transfer) {
     TransferEntity entity = new TransferEntity();
@@ -42,19 +73,41 @@ public class TransferEntity {
     entity.destinationAccount = transfer.getDestinationAccount().getValue();
     entity.amount = transfer.getAmount();
     entity.scheduledDate = transfer.getScheduledDate();
+    entity.transferDate = transfer.getTransferDate();
+
     if (transfer.getTransferFee() != null) {
       entity.transferFee = TransferFeeEntity.fromDomain(transfer.getTransferFee());
     }
+    if (transfer.getStatus() != null) {
+      entity.status = transfer.getStatus().getValue().getValue();
+    } else {
+      entity.status = TransferStatus.fromString("scheduled").getValue().getValue();
+    }
+
+    entity.createdAt = transfer.getCreatedAt();
+    entity.updatedAt = transfer.getUpdatedAt();
+    entity.deletedAt = transfer.getDeletedAt();
+
     return entity;
   }
 
   public Transfer toDomain() {
     TransferFee fee = transferFee != null ? transferFee.toDomain() : null;
-    return Transfer.create(
-        Account.create(sourceAccount),
-        Account.create(destinationAccount),
+    Transfer transfer = Transfer.from(
+        id,
+        sourceAccount,
+        destinationAccount,
         amount.doubleValue(),
         scheduledDate,
-        fee);
+        transferDate,
+        fee,
+        fee.getFixedFee(),
+        fee.getPercentageFee(),
+        status,
+        createdAt,
+        updatedAt,
+        deletedAt);
+
+    return transfer;
   }
 }
--- backend/src/main/java/com/johnnycarreiro/fts/infra/transfer/persistence/TransferFeeEntity.java
+++ backend/src/main/java/com/johnnycarreiro/fts/infra/transfer/persistence/TransferFeeEntity.java
@@ -1,26 +1,58 @@
 package com.johnnycarreiro.fts.infra.transfer.persistence;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 import com.johnnycarreiro.fts.domain.entities.transfer_fee.TransferFee;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "transfer_fees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TransferFeeEntity {
 
   @Id
+  @Column(name = "id", nullable = false)
   private String id;
+
+  @Column(name = "name", nullable = false)
   private String name;
+
+  @Column(name = "min_days", nullable = false)
   private int minDays;
+
+  @Column(name = "max_days")
   private Integer maxDays;
+
+  @Column(name = "fixed_fee", nullable = false)
   private BigDecimal fixedFee;
+
+  @Column(name = "percentage_fee", nullable = false)
   private BigDecimal percentageFee;
 
-  // Getters and Setters
+  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Instant createdAt;
+
+  @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Instant updatedAt;
+
+  @Column(name = "deleted_at", nullable = true, columnDefinition = "TIMESTAMP")
+  private Instant deletedAt;
+
+  @PreUpdate
+  public void preUpdate() {
+    this.updatedAt = Instant.now();
+  }
 
   public static TransferFeeEntity fromDomain(TransferFee transferFee) {
     var entity = new TransferFeeEntity();
@@ -30,10 +62,13 @@ public class TransferFeeEntity {
     entity.maxDays = transferFee.getMaxDays();
     entity.fixedFee = transferFee.getFixedFee();
     entity.percentageFee = transferFee.getPercentageFee();
+    entity.createdAt = transferFee.getCreatedAt();
+    entity.updatedAt = transferFee.getUpdatedAt();
+    entity.deletedAt = transferFee.getDeletedAt();
     return entity;
   }
 
   public TransferFee toDomain() {
-    return TransferFee.create(name, minDays, maxDays, fixedFee, percentageFee);
+    return TransferFee.from(id, name, minDays, maxDays, fixedFee, percentageFee, createdAt, updatedAt, deletedAt);
   }
 }
