e07689f67608dda17c669af65dda89352dbb0071
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-06 17:52:39 -0300

    refactor(domain)!: change return types to result type

--- backend/src/main/java/com/johnnycarreiro/fts/domain/services/TransferFeeCalculatorService.java
+++ backend/src/main/java/com/johnnycarreiro/fts/domain/services/TransferFeeCalculatorService.java
@@ -4,6 +4,9 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
+import com.johnnycarreiro.fts.core.domain.exceptions.DomainException;
+import com.johnnycarreiro.fts.core.domain.validation.Error;
+import com.johnnycarreiro.fts.core.utils.Result;
 import com.johnnycarreiro.fts.domain.entities.transfer_fee.TransferFee;
 
 public class TransferFeeCalculatorService {
@@ -13,22 +16,40 @@ public class TransferFeeCalculatorService {
   public TransferFeeCalculatorService() {
   }
 
-  public TransferFee calculateFee(Instant scheduledDateAt, Instant scheduleDateFor) {
-    if (this.feeRules == null) {
-      throw new IllegalArgumentException("No fee rule matches the provided dates.");
+  /**
+   * Calculates the fee for a transfer based on the scheduled and target dates.
+   *
+   * @param scheduledDateAt The date the transfer was scheduled.
+   * @param scheduleDateFor The date the transfer is intended for.
+   * @return A Result encapsulating either the matched TransferFee or an error
+   *         message.
+   */
+  public Result<TransferFee, DomainException> calculateFee(Instant scheduledDateAt, Instant scheduleDateFor) {
+    if (feeRules == null || feeRules.isEmpty()) {
+      return Result.error(DomainException.with(new Error("Fee rules are not configured.")));
     }
+
     long daysDifference = Duration.between(scheduledDateAt, scheduleDateFor).toDays();
 
     return feeRules.stream()
         .filter(rule -> (rule.getMinDays() <= daysDifference) &&
             (rule.getMaxDays() == null || rule.getMaxDays() >= daysDifference))
         .findFirst()
-        .orElseThrow(() -> new IllegalArgumentException("No fee rule matches the provided dates."));
+        .<Result<TransferFee, DomainException>>map(Result::success)
+        .orElseGet(() -> Result.error(DomainException.with(new Error("No fee rule matches the provided dates."))));
   }
 
-  public void setTransferFee(List<TransferFee> feeRules) {
+  /**
+   * Updates the fee rules for the service.
+   *
+   * @param feeRules The new list of fee rules.
+   * @return A Result indicating success or an error message.
+   */
+  public Result<Void, String> setTransferFee(List<TransferFee> feeRules) {
     if (feeRules == null || feeRules.isEmpty()) {
+      return Result.error("Fee rules cannot be null or empty.");
     }
     this.feeRules = feeRules;
+    return Result.success(null); // Success without a value
   }
 }
