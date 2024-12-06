f1a1efa542de1a864452ef9aa2bbf8321a8f8941
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-06 17:57:56 -0300

    fix(application): fixes cors polices

--- backend/src/main/java/com/johnnycarreiro/fts/BackendApplication.java
+++ backend/src/main/java/com/johnnycarreiro/fts/BackendApplication.java
@@ -2,6 +2,9 @@ package com.johnnycarreiro.fts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.johnnycarreiro.fts.configurations.WebServerConfig;
 
@@ -10,6 +13,16 @@ public class BackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WebServerConfig.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+      }
+    };
   }
 
 }
