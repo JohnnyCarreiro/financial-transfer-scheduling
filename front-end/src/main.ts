50e9b3082d4c3391355510460b368efd2d25709f
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2024-12-06 21:02:49 -0300

    refactor(app): remove unecessary import

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/index.css";
 import { transferService } from "@/config/ServiceProvider";
-import "@/config/vee-validate-config";
 
 createApp(App)
 	.provide("transferService", transferService)
